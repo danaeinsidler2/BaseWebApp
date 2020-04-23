bdd92a01d84605ef90ad02772e2938f0c2bf5ae5
danaeinsidler <you@example.com>
2020-04-23 12:44:38 -0400

    fixed broken image

--- project/js/main.js
+++ project/js/main.js
@@ -1,8 +1,8 @@
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+  $("#image").append('<img class="rounded-circle" src="images/borat.gif"/>');
+  $("p").html("Very nice, How much?!");
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
